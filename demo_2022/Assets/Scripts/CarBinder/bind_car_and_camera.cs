fc2a305214b7430924481aa2279988957719ada6
刘驰 <87454242+LiuChip@users.noreply.github.com>
2026-03-27 19:55:03 +0800

    AddBasicModels

---
+++ demo_2022/Assets/Scripts/CarBinder/bind_car_and_camera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bind_car_and_camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera ThirdParty = new Camera();
+
+    }
+}
